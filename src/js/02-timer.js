b31c4ba0ded1081ccc9a7997402306d842ba6d2c
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-29 20:55:19 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,17 @@
-// Описан в документации
-import flatpickr from 'flatpickr';
-// Дополнительный импорт стилей
-import 'flatpickr/dist/flatpickr.min.css';
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+// // Описан в документации
+// import flatpickr from 'flatpickr';
+// // Дополнительный импорт стилей
+// import 'flatpickr/dist/flatpickr.min.css';
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
 
-const calendar = document.querySelector('#datetime-picker');
-const flatpickr = flatpickr(calendar, options);
-console.log(calendar.value);
+// const calendar = document.querySelector('#datetime-picker');
+// const flatpickr = flatpickr(calendar, options);
+// console.log(calendar.value);
