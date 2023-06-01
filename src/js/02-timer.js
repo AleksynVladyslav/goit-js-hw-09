2b73461629153c2c6ccaee4b8ab5ef91418bc6eb
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-06-01 19:06:59 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,69 +1,66 @@
-// Описан в документации
 import flatpickr from 'flatpickr';
-// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
-//refs
-const inputRefs = document.querySelector('#datetime-picker');
-const buttonRefs = document.querySelector('button[data-start]');
-const daysRef = document.querySelector('span[data-days]');
-const hoursRef = document.querySelector('span[data-hours]');
-const minutesRef = document.querySelector('span[data-minutes]');
-const secondsRef = document.querySelector('span[data-seconds]');
-// const refs = {
-//   inputRefs: document.querySelector('#datetime-picker'),
-//   buttonRefs: document.querySelector('button[data-start]'),
-//   daysRef: document.querySelector('span[data-days]'),
-//   hoursRef: document.querySelector('span[data-hours]'),
-//   minutesRef: document.querySelector('span[data-minutes]'),
-//   secondsRef: document.querySelector('span[data-seconds]'),
-// };
+const refs = {
+  inputRef: document.querySelector('#datetime-picker'),
+  buttonRef: document.querySelector('button[data-start]'),
+  daysRef: document.querySelector('span[data-days]'),
+  hoursRef: document.querySelector('span[data-hours]'),
+  minutesRef: document.querySelector('span[data-minutes]'),
+  secondsRef: document.querySelector('span[data-seconds]'),
+};
 
-// const { inputRefs, buttonRefs, daysRef, hoursRef, minutesRef, secondsRef } =
-//   refs;
+const { inputRef, buttonRef, daysRef, hoursRef, minutesRef, secondsRef } = refs;
 
 let timerId = null;
 
-buttonRefs.disabled = true;
-// flatpickr
+buttonRef.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = new Date(selectedDates);
-    const currentDate = this.defaultDate;
+    const selectedDate = new Date(selectedDates[0]);
+    const currentDate = new Date();
 
     if (selectedDate < currentDate) {
       alert('Please choose a date in the future');
+      inputRef.value = ''; // Clear the input value
+      buttonRef.disabled = true; // Disable the button
       return;
     }
 
-    buttonRefs.disabled = false;
+    buttonRef.disabled = false;
   },
 };
-const flatpickr = flatpickr('#datetime-picker', options);
 
-//Listenern
-buttonRefs.addEventListener('click', onStartTimer);
+const datepicker = flatpickr(inputRef, options);
+
+buttonRef.addEventListener('click', onStartTimer);
 
 function onStartTimer() {
   timerId = setInterval(timer, 1000);
+  buttonRef.disabled = true;
 }
 
 function timer() {
-  const selectedDate = new Date(inputRefs.value);
-  const currentDate = Date.now();
+  const selectedDate = new Date(inputRef.value);
+  const currentDate = new Date();
   const diff = selectedDate - currentDate;
 
-  if (diff <= 1000) {
+  if (diff <= 0) {
     clearInterval(timerId);
+    updateCounter({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    console.log('Countdown completed!');
+    return;
   }
-  updateCouter(convertMs(diff));
+
+  updateCounter(convertMs(diff));
 }
 
-function updateCouter({ days, hours, minutes, seconds }) {
+function updateCounter({ days, hours, minutes, seconds }) {
   daysRef.textContent = addLeadingZero(days);
   hoursRef.textContent = addLeadingZero(hours);
   minutesRef.textContent = addLeadingZero(minutes);
@@ -75,19 +72,14 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
