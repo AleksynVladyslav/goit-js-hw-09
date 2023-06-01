a53f80cf0149ba3421cbe29ab4e2c34d96e90d4d
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-06-01 23:03:11 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs= {
+  submitRef: document.querySelector('button[type = submit]'),
+formRef: document.querySelector('.form'),
 }
+
+const {submitRef, formRef} = refs;
+
+console.log(formRef.elements)
+
+   submitRef.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(event){
+  console.log(event.currentTarget)  
+  event.preventDefault()
+
+return false
+}
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//     fulfillCallback(position, delay)
+//   } else {
+//     // Reject
+//     rejectCallback(position, delay)
+//   }
+// }
