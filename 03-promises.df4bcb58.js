7fc240a425138bab1b25ebf1c6693a95d1c586c6
AleksynVladyslav <120416059+AleksynVladyslav@users.noreply.github.com>
2023-06-01 21:03:44 +0000

    Deploying to gh-pages from @ AleksynVladyslav/goit-js-hw-09@a53f80cf0149ba3421cbe29ab4e2c34d96e90d4d 🚀

---
+++ 03-promises.df4bcb58.js
@@ -0,0 +1,2 @@
+const e={submitRef:document.querySelector("button[type = submit]"),formRef:document.querySelector(".form")},{submitRef:t,formRef:o}=e;console.log(o.elements),t.addEventListener("submit",function(e){return console.log(e.currentTarget),e.preventDefault(),!1});
+//# sourceMappingURL=03-promises.df4bcb58.js.map
