34c48a9b50989b9e5afe9c781b27151c62ade724
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-06-03 00:02:47 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,52 @@
-const refs= {
-  submitRef: document.querySelector('button[type = submit]'),
-formRef: document.querySelector('.form'),
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const del = Number(event.currentTarget.elements[0].value);
+  const step = Number(event.currentTarget.elements[1].value);
+  const amount = Number(event.currentTarget.elements[2].value);
+
+  createPromises(step, del, amount)
+    .then(results => {
+      results.forEach(result => {
+        if (result.status === 'fulfilled') {
+          const { step, delay } = result.value;
+          console.log(`✅ Fulfilled promise ${step} in ${delay}ms`);
+        } else if (result.status === 'rejected') {
+          const { step, delay } = result.reason;
+          console.log(`❌ Rejected promise ${step} in ${delay}ms`);
+        }
+      });
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-const {submitRef, formRef} = refs;
+function createPromise(step, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-console.log(formRef.elements)
-
-   submitRef.addEventListener('submit', onSubmitForm)
-
-function onSubmitForm(event){
-  console.log(event.currentTarget)  
-  event.preventDefault()
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ step, delay });
+      } else {
+        reject({ step, delay });
+      }
+    }, delay);
+  });
 }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    fulfillCallback(position, delay)
-  } else {
-    // Reject
-    rejectCallback(position, delay)
+function createPromises(step, delay, amount) {
+  const promises = [];
+
+  for (let i = 0; i < amount; i++) {
+    const promise = createPromise(1 + i, delay + i * step);
+    promises.push(promise);
   }
+
+  return Promise.allSettled(promises);
 }
